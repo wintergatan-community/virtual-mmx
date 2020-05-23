8018ff7685c95d03669553891c69f04655efe84b
Jack-Music <theonlymusicjack@gmail.com>
2020-05-23 20:56:20 +0200

    ADD: Basic react setup
    - add editorconfig
    - babel setup
    - react setup
    - add gitignore

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React, { ReactNode } from 'react';
+
+class App extends React.PureComponent {
+	render(): ReactNode {
+		return (
+			<div>
+				<h1>Welcome to vMMX</h1>
+			</div>
+		);
+	}
+}
+
+export default App;
