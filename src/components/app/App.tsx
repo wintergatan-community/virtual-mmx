37b6584fb07588089704c1398a3ebe6c28586ba5
Micah Switzer <micah.switzer.dev@outlook.com>
2020-05-25 14:32:07 -0500

    Add TransportControl component to App for testing

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,10 +1,16 @@
 import React from "react";
+import TransportControls from "../transport/TransportContols";
 // import { Program } from "vmmx-schema";
 // import "bootstrap/dist/css/bootstrap.min.css";
 // import Button from "react-bootstrap/Button";
 
 function App() {
-	return <div>Hello World!</div>;
+	return (
+		<div>
+			<h1>Hello World!</h1>
+			<TransportControls />
+		</div>
+	);
 }
 
 export default App;
