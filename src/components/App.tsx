22552fd63ed3ed28cc626e7a1a0218828e15dee9
Zak Griffin <zakgriffin1209@gmail.com>
2020-06-07 15:16:27 -0400

    starting to play with timing changes

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,16 @@
 import React from "react";
 import { ProgrammingWheel } from "./programEditor/ProgrammingWheel";
 import { TransportControls } from "./transport/TransportContols";
-import { observer } from "mobx-react";
 import { MockupLayout } from "./mockupLayout/MockupLayout";
+import { observer } from "mobx-react";
+// import { TimingTesting } from "./timingTesting/TimingTesting";
 
+// @observer
+// export class App extends Component {
+// 	render() {
+// 		return <TimingTesting />;
+// 	}
+// }
 export const App = observer(() => {
 	return (
 		<>
